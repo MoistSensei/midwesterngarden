0b7e3c4161c240cd4d76894d07c8a53e0b7ec51f
MoistSensei <pelkol@yahoo.com>
2025-05-12 16:32:07 -0500

    Add files via upload
    uploading

---
+++ js/GW script.js
@@ -0,0 +1,43 @@
+// JavaScript Document
+
+new 'Vivus' ('buena', {
+type: 'onebyone',
+duration: 300,
+delay: 150,
+animTimingFunction: 'Vivus.EASE'
+});
+
+new 'Vivus' ('devils', {
+type: 'onebyone',
+duration: 300,
+delay: 150,
+animTimingFunction: 'Vivus.EASE'
+});
+
+new 'Vivus' ('millstone', {
+type: 'onebyone',
+duration: 300,
+delay: 150,
+animTimingFunction: 'Vivus.EASE'
+});
+
+new 'Vivus' ('midgarden', {
+type: 'onebyone',
+duration: 300,
+delay: 100,
+animTimingFunction: 'Vivus.EASE'
+});
+
+new 'Vivus' ('woch', {
+type: 'onebyone',
+duration: 300,
+delay: 100,
+animTimingFunction: 'Vivus.EASE'
+});
+
+new 'Vivus' ('chicago', {
+type: 'onebyone',
+duration: 300,
+delay: 100,
+animTimingFunction: 'Vivus.EASE'
+});
